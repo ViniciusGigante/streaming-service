30596a52731230cfdcb54cb093b00eaefba4d1eb
ViniciusGigante <viniciusgigante539@gmail.com>
2025-08-29 22:07:18 -0300

    continuation of the implementation of movie posters on the home page

---
+++ src/app/Home/components/cardFilme.tsx
@@ -0,0 +1,9 @@
+export default function CardMovie(){
+    return(
+        <>
+            <div className="w-[256px] h-[359px] bg-gray-800 rounded-lg  m-2">
+    {/* Imagem do filme */}
+            </div>
+        </>
+    );
+};
--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,12 +1,24 @@
 import Sidebar from "./components/sidebar";
+import Header from "./components/header";
 import Banner from "./components/banner";
+import CardMovie from "./components/cardFilme";
 
 export default function HomePage() {
   return (
     <>
        <main className="flex w-full min-h-screen bg-[#b8b8b8]">
   <Sidebar />
-  <Banner />
+  <section className="h-[600px] min-h-[100vh] relative w-full bg-[#1E1E1E] text-white  "
+    style={{padding: '20px',backgroundImage: "url('/background-home.png')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",}}
+    >
+  <Header />
+  <Banner/>
+
+    <div></div>
+  
+</section>
 </main>
 
     </>
